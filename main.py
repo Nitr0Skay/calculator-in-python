3e94eccf5053c6320a36de476c624043b8cf5725
Nitr0Skay <cin.miko94@gmail.com>
2026-05-04 21:39:36 +0200

    Handle zero Division

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     if n2 == 0:
-        print("You cannot divide by Zero!")
-        return
+        raise ValueError("Division by Zero")
 
     return n1 / n2
 
