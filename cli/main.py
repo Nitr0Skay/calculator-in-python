51518907b44193e649ca41562cf2e1365120626f
Nitr0Skay <cin.miko94@gmail.com>
2026-05-05 21:12:07 +0200

    Remove redundant math functions

--- cli/main.py
+++ cli/main.py
@@ -1,55 +1,41 @@
-import random
+from calculator import Calculator as calc
 
-def add(n1, n2):
-    return n1 + n2
+def main():
+    repeat = True
+    print("Welcome to my Calculator")
+    step = 0
+    while repeat:
+        step += 1
+        number = get_number("Enter number: ")
+        operator = get_operator("Pick your operator: ")
 
-def subtract(n1, n2):
-    return n1 - n2
+        result = None
 
-def multiply(n1, n2):
-    return n1 * n2
+        match operator:
+            case "+":
+                result = add(number1, number2)
+            case "-":
+                result = subtract(number1, number2)
+            case "*":
+                result = multiply(number1, number2)
+            case "/" | ":":
+                result = divide(number1, number2)
+            case "%":
+                result = modulo(number1, number2)
+            case _:
+                print(random.choice(wrong_operator_messages))
 
-def divide(n1, n2):
-    if n2 == 0:
-        raise ValueError("Division by Zero")
+        if(result is not None):
+            print(f"Your result is {result}")
+        else:
+            print("Wasted")
 
-    return n1 / n2
+        shall_we_continue = input("Shall we continue? (y/n): ")
+        if shall_we_continue.lower() in ("y", "ye", "yes"):
+            print("Ah shit, here we go again...")
+        else:
+            print("Respect +")
+            repeat = False
 
-def modulo(n1, n2):
-    return n1 % n2
-
-repeat = True
-print("Welcome to my Calculator")
-step = 0
-while repeat:
-    step += 1
-    number = get_number("Enter number: ")
-    operator = get_operator("Pick your operator: ")
-
-    result = None
-
-    match operator:
-        case "+":
-            result = add(number1, number2)
-        case "-":
-            result = subtract(number1, number2)
-        case "*":
-            result = multiply(number1, number2)
-        case "/" | ":":
-            result = divide(number1, number2)
-        case "%":
-            result = modulo(number1, number2)
-        case _:
-            print(random.choice(wrong_operator_messages))
-
-    if(result is not None):
-        print(f"Your result is {result}")
-    else:
-        print("Wasted")
-
-    shall_we_continue = input("Shall we continue? (y/n): ")
-    if shall_we_continue.lower() in ("y", "ye", "yes"):
-        print("Ah shit, here we go again...")
-    else:
-        print("Respect +")
-        repeat = False
+if __name__ == "__main__":
+    main()
